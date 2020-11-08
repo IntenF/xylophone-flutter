127ab7347d76aaeb0e9845a20ea00cc22e8af47b
intenf from bigboss <taichi.nakamura2104@gmail.com>
2020-11-08 14:23:27 +0900

    終了

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  var color;
+  Widget buttonKey(int soundNumber) {
+    soundNumber = soundNumber % 7;
+    List colorList = [
+      Colors.red,
+      Colors.orange,
+      Colors.yellow,
+      Colors.lightGreen,
+      Colors.green,
+      Colors.blue,
+      Colors.purple,
+    ];
+    return Expanded(
+      child: FlatButton(
+        color: colorList[soundNumber],
+        onPressed: () {
+          final player = AudioCache();
+          player.play('note${soundNumber + 1}.wav');
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: List.generate(7, (index) => buttonKey(index)),
+          ),
         ),
       ),
     );
